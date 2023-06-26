eca5cf0bef027071a4202ee9d3d630403a7babbb
Irina Antonishina <111454571+IrinaAntonishina@users.noreply.github.com>
2023-06-26 17:27:58 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+
+const createMarkup = galleryItems.map(({preview, original, description}) => {
+    return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+       <img
+         class="gallery__image"
+         src="${preview}"
+         data-source="${original}"
+         alt="${description}"
+       />
+     </a>
+     </li>`
+}).join('');
+
+galleryList.insertAdjacentHTML('afterbegin', createMarkup)
+
+
+galleryList.addEventListener('click', evt => {
+    evt.preventDefault();
+    const linkEl = evt.target.closest('.gallery__link')
+    if(linkEl){
+        const href = linkEl.getAttribute('href')
+        const instance = basicLightbox.create(createModalMarkup(href))
+        instance.show();
+        linkEl.addEventListener('keydown', (event) =>{
+            if(event.key === 'Escape'){
+                instance.close()
+            }
+            
+            })
+    }else{return}
+    
+        
+        
+})
+
+function createModalMarkup (link) {
+    return `<img
+    class="gallery__image"
+    src="${link}"
+    data-source="${link}"
+  />`
+}
+
