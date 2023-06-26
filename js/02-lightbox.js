9ce69a2b00cb12cf915db72c70cbae796dbdca9a
Irina Antonishina <111454571+IrinaAntonishina@users.noreply.github.com>
2023-06-26 18:02:33 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
+const createMarkup = galleryItems.map(({preview, original, description}) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>`
+}).join('');
 
+galleryList.insertAdjacentHTML('afterbegin', createMarkup)
 console.log(galleryItems);
+
+galleryList.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    const imgEl = evt.target.closest('.gallery__link')
+    if(!imgEl){
+        return;
+    } 
+
+})
+new SimpleLightbox('.gallery a', { captionsData:"alt", captionDelay:250, })
